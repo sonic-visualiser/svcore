8f61bb5f90522aae911769bb991e98e31620e9ee
Chris Cannam <cannam@all-day-breakfast.com>
2006-03-20 11:40:39 +0000

    * Set indent-tabs-mode to nil in Emacs mode direction

--- base/Layer.h
+++ base/Layer.h
@@ -1,5 +1,5 @@
 
-/* -*- c-basic-offset: 4 -*-  vi:set ts=8 sts=4 sw=4: */
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*-  vi:set ts=8 sts=4 sw=4: */
 
 /*
     A waveform viewer and audio annotation editor.
