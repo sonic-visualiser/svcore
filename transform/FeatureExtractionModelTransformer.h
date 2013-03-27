5f64032addfe55573e9b732aa452da44ae3baf29
Chris Cannam <cannam@all-day-breakfast.com>
2013-03-27 15:27:33 +0000

    Fix calculation of timestamps for fixed-rate features

--- transform/FeatureExtractionModelTransformer.h
+++ transform/FeatureExtractionModelTransformer.h
@@ -40,6 +40,7 @@ protected:
 
     Vamp::Plugin *m_plugin;
     Vamp::Plugin::OutputDescriptor *m_descriptor;
+    int m_fixedRateFeatureNo; // to assign times to FixedSampleRate features
     int m_outputNo;
 
     void createOutputModel();
