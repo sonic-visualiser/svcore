39dccfa0dc805417443ecfa15435b6af4b32dd13
Chris Cannam <cannam@all-day-breakfast.com>
2014-06-13 16:35:15 +0100

    Remove some debug out

--- data/fileio/CodedAudioFileReader.cpp
+++ data/fileio/CodedAudioFileReader.cpp
@@ -46,7 +46,7 @@ CodedAudioFileReader::CodedAudioFileReader(CacheMode cacheMode,
     m_max(0.f),
     m_gain(1.f)
 {
-    cerr << "CodedAudioFileReader::CodedAudioFileReader: rate " << targetRate << ", normalised = " << normalised << endl;
+    SVDEBUG << "CodedAudioFileReader::CodedAudioFileReader: rate " << targetRate << ", normalised = " << normalised << endl;
 
     m_frameCount = 0;
     m_sampleRate = targetRate;
@@ -446,7 +446,6 @@ CodedAudioFileReader::getInterleavedFrames(size_t start, size_t count,
     }
 
     if (m_normalised) {
-        cerr << "CodedAudioFileReader::getInterleavedFrames: normalised, gain = " << m_gain << endl;
         for (int i = 0; i < (int)(count * m_channelCount); ++i) {
             frames[i] *= m_gain;
         }
